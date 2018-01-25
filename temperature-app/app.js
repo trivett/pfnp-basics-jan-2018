3f79850a6a584992d2f74c836207733e146a5810
Vincent Trivett <trivett@gmail.com>
2018-01-24 23:40:27 -0500

    finished in class temperature app

--- temperature-app/app.js
+++ temperature-app/app.js
@@ -1,2 +1,14 @@
 // paste your function that converts farenheit to celcius here:
 
+function farenheitToCelsius(f) {
+  let step1 = f - 32;
+  let step2 = step1 * 5;
+  let c = step2 / 9;
+  return c;
+}
+
+$('#temperature-conversion-form').on('submit', function () {
+  let f = $('#farenheit-input').val();
+  let c = farenheitToCelsius(f);
+  $('#answer').html(c);
+});
